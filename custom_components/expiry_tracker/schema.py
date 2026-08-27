d183820fe53839e0787ed3ab48bdfd956c576b72
conorod1992 <58264595+conorod1992@users.noreply.github.com>
2026-08-27 23:45:38 +0100

    Expose action wording fields

--- custom_components/expiry_tracker/schema.py
+++ custom_components/expiry_tracker/schema.py
@@ -3,6 +3,17 @@
 import voluptuous as vol
 from homeassistant.helpers import config_validation as cv
 
+_ACTION_TYPES = [
+    "renew",
+    "replace",
+    "review",
+    "retest",
+    "reregister",
+    "cancel",
+    "check",
+    "custom",
+]
+
 CREATE_FIELDS = {
     vol.Required("name"): cv.string,
     vol.Required("expiry_date"): cv.string,
@@ -13,6 +24,8 @@ CREATE_FIELDS = {
     vol.Optional("important", default=False): cv.boolean,
     vol.Optional("expose_entity", default=False): cv.boolean,
     vol.Optional("requires_action", default=True): cv.boolean,
+    vol.Optional("action_type", default="renew"): vol.In(_ACTION_TYPES),
+    vol.Optional("custom_action_label"): vol.Any(None, cv.string),
     vol.Optional("actionable_mode", default="offset"): vol.In(["immediate", "offset", "date"]),
     vol.Optional("actionable_offset_value", default=30): vol.All(
         vol.Coerce(int), vol.Range(min=0, max=36500)
